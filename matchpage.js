66d39ed2b12403d15774fadd04ba4bfe5de5ce07
Adji Mulyadi <117246848+dudungelo@users.noreply.github.com>
2024-01-13 14:39:27 +0700

    Update matchpage.js

--- matchpage.js
+++ matchpage.js
@@ -1,5 +1,4 @@
 /*<![CDATA[*/
-function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t  window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n  "all" }) }loadCSS("https://cdn.jsdelivr.net/gh/dudungelo/match-post@main/matchpost.css");
  var current_channel; $(document).ready(function() {  /* Channel Link Class */
   $('a.crcl-link').on('click', function(event) {   $('a.crcl-link').removeClass('active');   $(this).addClass('active');   current_channel = $(this).attr('href');
    $('input#current_channel').val(current_channel);   console.log(current_channel);  });
