855135e46a5d0d641cd8165de998dbff4de70dc9
Adji Mulyadi <117246848+dudungelo@users.noreply.github.com>
2024-01-13 14:30:59 +0700

    Create matchpage.js

---
+++ matchpage.js
@@ -0,0 +1,18 @@
+/*<![CDATA[*/
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t  window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n  "all" }) }loadCSS("https://cdn.jsdelivr.net/gh/dudungelo/match-post@main/matchpost.css");
+ var current_channel; $(document).ready(function() {  /* Channel Link Class */
+  $('a.crcl-link').on('click', function(event) {   $('a.crcl-link').removeClass('active');   $(this).addClass('active');   current_channel = $(this).attr('href');
+   $('input#current_channel').val(current_channel);   console.log(current_channel);  });
+  /* Channel List */ 
+   $('#cr-broadcast-list .cr-boardcasts').click(function(event) {   $(this).addClass('active').siblings('.cr-boardcasts').removeClass('active');   if( $(this).next('.cr-channels').attr('style') == 'display: block;' ) {
+    $(this).removeClass('active');    $(this).next('.cr-channels').slideUp();   } else {    $(this).next('.cr-channels').slideDown().siblings('.cr-channels').slideUp();
+   }  }); 
+ });  const date = new Date().toUTCString();
+  const extractedDateTime = date.slice(5, 16);  document.getElementById("extractedDateTime").innerHTML = "Today Match Schedule: " + extractedDateTime;
+    const besok = () => {  let hariIni = new Date();
+  hariIni.setDate(hariIni.getDate() + 1);  const namaBulan = [    "Jan", "Feb", "Mar", "Apr", "Mei", "Jun",
+    "Jul", "Agu", "Sep", "Okt", "Nov", "Des"  ];  let dd = String(hariIni.getDate()).padStart(2, '0');
+  let mm = namaBulan[hariIni.getMonth()];  let yyyy = hariIni.getFullYear();  let tanggalBesok = dd + ' ' + mm + ' ' + yyyy;
+  return tanggalBesok;};
+document.querySelector(".tomorrow-date").innerHTML = besok();
+//]]>
